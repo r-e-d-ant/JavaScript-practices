e3656fee8e8db0cc2b98b0748ec31c11c75fe75f
r-e-d-ant <mugishathierry65@gmail.com>
2021-09-14 14:35:14 +0200

    Match All Non-Numbers

--- practice.js
+++ practice.js
@@ -1129,6 +1129,18 @@ let movieName = "2001: A Space Odyssey";
 let numRegex = /\d/ig; // Change this line
 let result = movieName.match(numRegex).length;
 
+// Match All Non-Numbers
+
+/*
+- The last challenge showed how to search for digits using the shortcut \d with a lowercase d.
+You can also search for non-digits using a similar shortcut that uses an uppercase D instead.
+The shortcut to look for non-digit characters is \D. This is equal to the character class [^0-9],
+which looks for a single character that is not a number between zero and nine.
+*/
+
+let movieName = "2001: A Space Odyssey";
+let noNumRegex = /\D/ig; // Change this line
+let result = movieName.match(noNumRegex).length;
 
 
 
